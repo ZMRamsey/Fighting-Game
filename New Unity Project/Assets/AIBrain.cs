738f46be37b9c99867a47433c9d896e14d8037c4
Harlow <laylif@hotmail.com>
2022-01-20 19:50:05 +0000

    Improved AI

--- New Unity Project/Assets/AIBrain.cs
+++ New Unity Project/Assets/AIBrain.cs
@@ -10,11 +10,10 @@ public class AIBrain : MonoBehaviour
     float tick;
 
     void Start() {
-        _handler = GetComponent<InputHandler>();    
+        _handler = GetComponent<InputHandler>();
     }
 
-    void Update()
-    {
+    void Update() {
         //if(_handler.GetState() == InputState.ai && _shuttle.IsBallActive()) {
         //    print("Test");
         //    Vector3 targetPosition = _shuttle.transform.position + _shuttle.GetVelocity();
@@ -26,8 +25,9 @@ public class AIBrain : MonoBehaviour
         Vector3 targetPosition = _shuttle.transform.position + _shuttle.GetVelocity() / 4;
 
         if (tick > 0.05f) {
+            _handler._inputX = 0;
 
-            if (transform.position.x < targetPosition.x && transform.position.x < -1) {
+            if (transform.position.x < targetPosition.x && targetPosition.x < -1) {
                 _handler._inputX = 1;
             }
 
@@ -35,26 +35,43 @@ public class AIBrain : MonoBehaviour
                 _handler._inputX = -1;
             }
 
-            if ((_shuttle.GetVelocity().y > 0 && _shuttle.transform.position.y > transform.position.y) || _shuttle.transform.position.y > 6.8f) {
-                _handler._jumpHeld = transform.position.y < _shuttle.transform.position.y;
+
+            _handler._jumpHeld = transform.position.y < _shuttle.transform.position.y;
+
+            if ((_shuttle.transform.position.y < targetPosition.y && _shuttle.transform.position.y > 1.2f)  || _shuttle.transform.position.y > 6.8f) {
                 _handler._jumpInput = true;
             }
             else {
                 _handler._jumpInput = false;
             }
 
-            if (Vector3.Distance(transform.position, _shuttle.transform.position) < _shuttle.GetVelocity().magnitude / 4) {
-                int rand = Random.Range(0, 3);
 
-                if (rand == 2) {
+            if (Vector3.Distance(transform.position, targetPosition) < _shuttle.GetVelocity().magnitude / 4) {
+                int rand = Random.Range(0, 3);
+                int rndDec = 0;
+
+                if (rand == 2 && _shuttle.transform.position.y > 1.2f) {
                     _handler._chipInput = true;
                 }
                 else {
                     if (transform.position.y < 1.2f) {
-                        _handler._driveInput = true;
+                        rndDec = Random.Range(0, 2);
+                        if (rndDec == 1) {
+                            _handler._smashInput = true;
+                        }
+                        else {
+                            _handler._dropInput = true;
+                        }
+
                     }
                     else {
-                        _handler._dropInput = true;
+                        rndDec = Random.Range(0, 2);
+                        if (rndDec == 1) {
+                            _handler._driveInput = true;
+                        }
+                        else {
+                            _handler._dropInput = true;
+                        }
                     }
                 }
             }
