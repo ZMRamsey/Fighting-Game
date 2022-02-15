c41d2015adcdfe7a67b4c05741407084b93a741d
Shaun Kennie <u1852739@hud.ac.uk>
2022-02-15 17:49:12 +0000

    bug fixes and removed low speed collisions

--- New Unity Project/Assets/Scripts/Hurtbox.cs
+++ New Unity Project/Assets/Scripts/Hurtbox.cs
@@ -29,14 +29,14 @@ public class Hurtbox : MonoBehaviour
                 ScoreManager.Get().UpdateScore(transform.root.GetComponent<FighterController>().GetFilter().ToString(), "KO");
                 GameManager.Get().KOEvent();
             }
-            else if (hurt.GetSpeedPercent() >= .3f) {
-                //Debug.Log("Sonic_Spring_Noise.MP3");
-                transform.root.GetComponent<FighterController>().SetFighterStance(FighterStance.blow);
-                //hurt.Shoot(hurt.GetVelocity() / -10, new Vector3(), false, false, FighterFilter.both);
-                hurt.Bounce(axis*2);
-                transform.root.GetComponent<FighterController>().ReduceMeter(5f);
-                hurt.SetOwner(transform.root.GetComponent<FighterController>());
-            }
+            //else if (hurt.GetSpeedPercent() >= .3f) {
+            //    //Debug.Log("Sonic_Spring_Noise.MP3");
+            //    transform.root.GetComponent<FighterController>().SetFighterStance(FighterStance.blow);
+            //    //hurt.Shoot(hurt.GetVelocity() / -10, new Vector3(), false, false, FighterFilter.both);
+            //    hurt.Bounce(axis*2);
+            //    transform.root.GetComponent<FighterController>().ReduceMeter(5f);
+            //    hurt.SetOwner(transform.root.GetComponent<FighterController>());
+            //}
             else {
                 //Debug.Log("We Tech Those");
             }
