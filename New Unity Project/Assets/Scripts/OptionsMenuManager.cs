286d9bfde9165dedd83bf71283bcbd6b5663e7d4
MatthewTAustwick <matthew.t.austwick@outlook.com>
2022-02-22 09:39:56 +0000

    I want to cry part 2

--- New Unity Project/Assets/Scripts/OptionsMenuManager.cs
+++ New Unity Project/Assets/Scripts/OptionsMenuManager.cs
@@ -59,6 +59,11 @@ public class OptionsMenuManager : MonoBehaviour
         }
     }
 
+    public void ControlCharacterSelection()
+    {
+
+    }
+
     void HighlightSelectedCharacter()
     {
         //characters[_selectionIndex].SetActive(true);
