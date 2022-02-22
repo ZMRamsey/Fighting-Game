647f7cdf721880038ecbb064e162e4572aece468
MatthewTAustwick <matthew.t.austwick@outlook.com>
2022-02-22 15:21:55 +0000

    More work on loading

--- New Unity Project/Assets/LoadingTextGenerator.cs
+++ New Unity Project/Assets/LoadingTextGenerator.cs
@@ -1,11 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class LoadingTextGenerator : MonoBehaviour
 {
+    public int _maxQuotes;
+    int randQuoteNum;
     public string[] loadingFacts;
-    string fact;
 
-   
+    private TextMeshProUGUI scrollingText;
+
+    public void Start()
+    {
+        scrollingText = GetComponent<TextMeshProUGUI>();
+        ___LoadingUpdateText();
+    }
+
+    public void ___LoadingUpdateText()
+    {
+        randQuoteNum = Random.Range(0, _maxQuotes);
+        scrollingText.text = loadingFacts[randQuoteNum];
+        Debug.Log(randQuoteNum.ToString());
+    }
+
+    private void Update()
+    {
+        
+    }
 }
