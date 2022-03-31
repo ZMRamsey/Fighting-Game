84a99cc77339fcf149d60d5cde0fcc3216ac0cfc
Shaun Kennie <shaunkennie@gmail.com>
2022-03-31 17:37:02 +0100

    some menu stuff and new font

--- New Unity Project/Assets/Scripts/New Menu/MainMenuPage.cs
+++ New Unity Project/Assets/Scripts/New Menu/MainMenuPage.cs
@@ -1,9 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenuPage : MonoBehaviour
 {
+    [SerializeField] Button _backButton;
+
+    public void Start()
+    {
+        if (_backButton)
+        {
+            _backButton.onClick.AddListener(() => MainMenuSystem.Get().SetPage(0));
+        }
+    }
+
     public void EnablePage()
     {
         gameObject.SetActive(true);
--- New Unity Project/Assets/Scripts/New Menu/MainMenuSystem.cs
+++ New Unity Project/Assets/Scripts/New Menu/MainMenuSystem.cs
@@ -1,14 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenuSystem : MonoBehaviour
 {
+    public static MainMenuSystem _instance;
     [SerializeField] MainMenuPage[] _mainMenuPages;
+    [SerializeField] Button[] _mainMenuButtons;
+
+    private void Awake()
+    {
+        _instance = this;
+    }
 
     void Start()
     {
         SetPage(0);
+        for (int i = 0; i < _mainMenuButtons.Length; i++)
+        {
+            int steve = i + 1;
+            _mainMenuButtons[i].onClick.AddListener(() => SetPage(steve));
+        }
     }
 
     public void SetPage(int ID)
@@ -32,5 +45,10 @@ public class MainMenuSystem : MonoBehaviour
     public void LoadToCharacterSelect()
     {
 
+    }
+
+    public static MainMenuSystem Get()
+    {
+        return _instance;
     }
 }
