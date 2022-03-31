0537921a29da12383b225c7ffecbaadac781a389
CrimsonHarlow <97447290+CrimsonHarlow@users.noreply.github.com>
2022-03-31 18:07:49 +0100

    :wink:

--- New Unity Project/Assets/Scripts/FighterUI.cs
+++ New Unity Project/Assets/Scripts/FighterUI.cs
@@ -6,13 +6,14 @@ using UnityEngine.UI;
 public class FighterUI : MonoBehaviour
 {
     [SerializeField] Image _mainBar;
-    [SerializeField] Image _fill;
-    [SerializeField] Image _arrowPosBar;
-    [SerializeField] Image _arrowNegBar;
-    [SerializeField] Image _superEffectBar;
-    [SerializeField] Gradient _superPulse;
+    //[SerializeField] Image _fill;
+    //[SerializeField] Image _arrowPosBar;
+    //[SerializeField] Image _arrowNegBar;
+    //[SerializeField] Image _superEffectBar;
+    //[SerializeField] Gradient _superPulse;
     [SerializeField] GameObject _flash;
     float _targetAmount;
+    float _currentValue;
 
     float _positiveTimer;
     float _negativeTimer;
@@ -20,71 +21,79 @@ public class FighterUI : MonoBehaviour
 
     void Start() {
         SetBarValue(0);
-        _arrowPosBar.material.SetFloat("_Opacity", 0);
-        _arrowNegBar.material.SetFloat("_Opacity", 0);
-        _superEffectBar.material.SetFloat("_Opacity", 0);
-        _fill.color = Color.white;
+        _flash.SetActive(false);
+        //_arrowPosBar.material.SetFloat("_Opacity", 0);
+        //_arrowNegBar.material.SetFloat("_Opacity", 0);
+        //_superEffectBar.material.SetFloat("_Opacity", 0);
+       // _fill.color = Color.white;
     }
 
     void Update() {
-        _mainBar.fillAmount = Mathf.Lerp(_mainBar.fillAmount, _targetAmount, Time.deltaTime * 2f);
-        _arrowPosBar.fillAmount = _mainBar.fillAmount;
-        _arrowNegBar.fillAmount = _mainBar.fillAmount;
-        _superEffectBar.fillAmount = _mainBar.fillAmount;
+        _currentValue = Mathf.Lerp(_currentValue, _targetAmount, Time.deltaTime * 2);
+        _mainBar.fillAmount = Mathf.Lerp(0.42f, 0.83f, _currentValue);
+        //_arrowPosBar.fillAmount = _mainBar.fillAmount;
+        //_arrowNegBar.fillAmount = _mainBar.fillAmount;
+        //_superEffectBar.fillAmount = _mainBar.fillAmount;
 
-        if (_positiveTimer > 0.0f) {
-            _positiveTimer -= Time.deltaTime;
-        }
+        //if (_positiveTimer > 0.0f) {
+        //    _positiveTimer -= Time.deltaTime;
+        //}
 
-        float pos = _arrowPosBar.material.GetFloat("_Opacity");
-        float neg = _arrowNegBar.material.GetFloat("_Opacity");
-        float super = _superEffectBar.material.GetFloat("_Opacity");
+        //float pos = _arrowPosBar.material.GetFloat("_Opacity");
+        //float neg = _arrowNegBar.material.GetFloat("_Opacity");
+        //float super = _superEffectBar.material.GetFloat("_Opacity");
 
-        if (_positiveTimer <= 0.0f && pos > 0) {
-            pos -= Time.deltaTime;
-            _arrowPosBar.material.SetFloat("_Opacity", pos);
-        }
+        //if (_positiveTimer <= 0.0f && pos > 0) {
+        //    pos -= Time.deltaTime;
+        //    _arrowPosBar.material.SetFloat("_Opacity", pos);
+        //}
 
-        if (_negativeTimer <= 0.0f && neg > 0) {
-            neg -= Time.deltaTime;
-            _arrowNegBar.material.SetFloat("_Opacity", neg);
-        }
+        //if (_negativeTimer <= 0.0f && neg > 0) {
+        //    neg -= Time.deltaTime;
+        //    _arrowNegBar.material.SetFloat("_Opacity", neg);
+        //}
 
-        if (_targetAmount == 1) {
-            _superTimer += Time.deltaTime;
-            if (_superTimer > 1) {
-                _superTimer = 0.0f;
-            }
+        //if (_targetAmount == 1) {
+        //    _superTimer += Time.deltaTime;
+        //    if (_superTimer > 1) {
+        //        _superTimer = 0.0f;
+        //    }
 
-            _fill.color = _superPulse.Evaluate(_superTimer);
-        }
+        //    _fill.color = _superPulse.Evaluate(_superTimer);
+        //}
     }
 
     public void SetBarValue(float value) {
-        float oldValue = _targetAmount;
         _targetAmount = value;
 
-        if (_targetAmount >= 1) {
+        if(value == 0)
+        {
+            _currentValue = 0;
+        }
+
+        if (_targetAmount >= 1)
+        {
             _flash.SetActive(true);
-            _superEffectBar.material.SetFloat("_Opacity", 1);
+            //_superEffectBar.material.SetFloat("_Opacity", 1);
             return;
         }
-        else {
+        else
+        {
             _flash.SetActive(false);
-            _superEffectBar.material.SetFloat("_Opacity", 0);
+            //_superEffectBar.material.SetFloat("_Opacity", 0);
         }
 
-        if (_fill.color != Color.white) {
-            _fill.color = Color.white;
-        }
+        //if (_fill.color != Color.white) {
+        //    _fill.color = Color.white;
+        //}
 
-        if (_targetAmount > oldValue) {
-            _arrowPosBar.material.SetFloat("_Opacity", 1);
-            _positiveTimer = 0.5f;
-        }
-        else {
-            _arrowNegBar.material.SetFloat("_Opacity", 1);
-            _positiveTimer = 0.5f;
-        }
+        //if (_targetAmount > oldValue) {
+        //    _arrowPosBar.material.SetFloat("_Opacity", 1);
+        //    _positiveTimer = 0.5f;
+        //}
+        //else {
+        //    _arrowNegBar.material.SetFloat("_Opacity", 1);
+        //    _positiveTimer = 0.5f;
+        //}
     }
 }
